5067fa9d7faf1389868de16e4827d1de307d8c99
democreation1 <123737422+democreation1@users.noreply.github.com>
2023-03-20 17:08:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,15 +73,3 @@ resource "aws_s3_bucket" "four" {
 }
 
 
-resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "aurora-cluster-demo"
-  engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.10.2"
-  availability_zones      = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  database_name           = "mydb"
-  master_username         = "foo"
-  master_password         = "Raham@#444555"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-}
-
