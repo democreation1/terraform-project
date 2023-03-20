4d40b0a380680659e500fa4dc19bcd1b583ff25e
democreation1 <123737422+democreation1@users.noreply.github.com>
2023-03-20 16:51:12 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
+  name               = "harshii-terraform-elb"
   availability_zones = ["ap-south-1a", "ap-south-1b"]
 
   listener {
@@ -10,13 +10,14 @@ resource "aws_elb" "bar" {
   }
 
   health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
+    healthy_threshold   = 2  #this is ntg but if we connected to both the servers without any interrupotion then it is healty
+    unhealthy_threshold = 2  #this is ntg but if we send 2rqsts and we don't get connected to website then it is unhealthy 
+    timeout             = 5  #this is ntg but if we don't get the rqst from server in 5sec then it is time out
     target              = "HTTP:80/"
-    interval            = 30
+    interval            = 30 #the time gap b/w the health checks
   }
-
+    
+  #we have created loadbalancer and we need to create instance for it
   instances                 = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
   cross_zone_load_balancing = true
   idle_timeout              = 400
