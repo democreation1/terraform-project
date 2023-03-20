7b3e723e7b2e30290a9831eb638ab445f7e4558d
democreation1 <123737422+democreation1@users.noreply.github.com>
2023-03-20 16:56:30 +0530

    tf

--- main.tf
+++ main.tf
@@ -57,14 +57,14 @@ resource "aws_security_group" "three" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]   #Anywhere
   }
 
   egress {   #outbound
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]     #Anywhere
   }
 }
 
